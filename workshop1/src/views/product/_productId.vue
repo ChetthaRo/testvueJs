293d3d2f3361c3209955433f6fe6705bbe4f3b89
Chettha Rodthong <chettha.r@manpow.co>
2024-02-12 23:58:05 +0700

    แก้ไข swal

--- workshop1/src/views/product/_productId.vue
+++ workshop1/src/views/product/_productId.vue
@@ -132,7 +132,7 @@ export default {
       })
       .catch((error) => {
         console.log("error:", error);
-        this.$swal({
+        Swal.fire({
           icon: " error",
           title: "เกิดข้อผิดพลาด",
           text: error.response?.data?.message,
